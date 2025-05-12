154948a79ef0d425d15e21cc09525b5af92ff3aa
William Salazar <wwwwesh@gmail.com>
2025-05-12 10:20:27 -0600

    Adds API route to logout

--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -6,7 +6,11 @@ export default function LogoutButton() {
   const router = useRouter()
 
   const handleLogout = async () => {
+    await fetch('/api/logout', {
+      method: 'POST',
+    })
 
+    router.push('/login')
   }
 
   return (
